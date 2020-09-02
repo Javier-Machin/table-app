4c3e42e20e75e28cd00cfc9e6753f2ab48e07fc2
Javier M <javier_machin_acosta@hotmail.com>
2020-09-02 19:51:46 +0100

    Updates

---
+++ precache-manifest.438b6137b5a7150d1c4a16b056af481c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0beb15c602935603b23d53746dfe4597",
+    "url": "/table-app/index.html"
+  },
+  {
+    "revision": "dbd5009d19876584df06",
+    "url": "/table-app/static/css/main.67185310.chunk.css"
+  },
+  {
+    "revision": "51f671026b8d02f99c3c",
+    "url": "/table-app/static/js/2.afdc9f00.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/table-app/static/js/2.afdc9f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbd5009d19876584df06",
+    "url": "/table-app/static/js/main.bd30b242.chunk.js"
+  },
+  {
+    "revision": "fa0eee9f4efbc3b34d74",
+    "url": "/table-app/static/js/runtime-main.7d3c62d7.js"
+  }
+]);
