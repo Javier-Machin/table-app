77c4be2f455f4dd7507984540128ab979113ebbb
Javier M <javier_machin_acosta@hotmail.com>
2020-09-02 20:02:21 +0100

    fix: default test

--- src/tests/App.spec.js
+++ src/tests/App.spec.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 import { render } from '@testing-library/react';
+import reducer from '../store/reducers';
 import App from '../App';
 
+const middleware = [thunk];
+const store = createStore(reducer, applyMiddleware(...middleware));
+
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/oi lad/i);
+  const { getByText } = render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  const linkElement = getByText(/total rows/i);
   expect(linkElement).toBeInTheDocument();
 });
